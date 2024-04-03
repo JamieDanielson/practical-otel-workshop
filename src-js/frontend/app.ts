f7a40b2f458ccac5f8c9edcb581ec157c868e9fc
JamieDanielson <jamieedanielson@gmail.com>
2024-04-03 18:38:48 -0400

    use nodefetch instead of http

--- src-js/frontend/app.ts
+++ src-js/frontend/app.ts
@@ -1,27 +1,20 @@
-import express, { Request, Express } from 'express';
-import http from 'http';
+/* eslint-disable @typescript-eslint/no-base-to-string */
+import express, { Express, Request, Response } from 'express';
+import nodefetch from 'node-fetch';
 
 const PORT: number = parseInt(process.env.PORT || '5004');
 const app: Express = express();
 
-app.get('/', async (req, res) => {
-    const firstname = req.query.firstname;
-    const surname = req.query.surname;
-    let responseBody = '';
-    http.get({
-      host: 'localhost',
-      port: 5005,
-      path: `/profile?firstname=${firstname}&surname=${surname}`,
-    }, (response:any) => {
-      var body = "";
-      response.on('data', (chunk:never) => body += chunk);
-      response.on('end', () => {
-        responseBody = body;
-        var json:PersonInfo = JSON.parse(responseBody) as PersonInfo;
-        res.send(`hello ${json.name} you're ${json.age} years old`);
-      });
-    });
-
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+app.get('/', async (req: Request, res: Response) => {
+  const firstname = req.query?.firstname.toString();
+  const surname = req.query?.surname.toString();
+  const person = await nodefetch(
+    `http://localhost:5005/profile?firstname=${firstname}&surname=${surname}`,
+  );
+  const response = await person.text();
+  const json: PersonInfo = JSON.parse(response) as PersonInfo;
+  res.send(`hello ${json.name} you're ${json.age} years old`);
 });
 
 app.listen(PORT, () => {
@@ -29,6 +22,6 @@ app.listen(PORT, () => {
 });
 
 class PersonInfo {
-    name: string = '';
-    age: number = 0;
+  name: string = '';
+  age: number = 0;
 }
