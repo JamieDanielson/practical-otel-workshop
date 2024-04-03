8bed7e20c718d300479223f09d35282fea592557
JamieDanielson <jamieedanielson@gmail.com>
2024-04-03 18:36:17 -0400

    typescripty things for backend app

--- src-js/backend/app.ts
+++ src-js/backend/app.ts
@@ -1,17 +1,20 @@
-import express, { Request, Express } from 'express';
+/* eslint-disable @typescript-eslint/no-base-to-string */
+import express, { Express, Request, Response } from 'express';
 import { trace } from '@opentelemetry/api';
 
 const PORT: number = parseInt(process.env.PORT || '5005');
 const app: Express = express();
 
-app.get('/profile', (req, res) => {
-    const span = trace.getActiveSpan();
-    const age = Math.floor(Math.random() * 100);
-    span?.setAttribute('age', age);
+app.get('/profile', (req: Request, res: Response) => {
+  const span = trace.getActiveSpan();
+  const age = Math.floor(Math.random() * 100);
+  span?.setAttribute('age', age);
+  const firstname = req.query?.firstname?.toString();
+  const surname = req.query?.surname?.toString();
   res.send({
-    name: `${req.query.firstname} ${req.query.surname}`,
-    age
-  })
+    name: `${firstname} ${surname}`,
+    age,
+  });
 });
 
 app.listen(PORT, () => {
